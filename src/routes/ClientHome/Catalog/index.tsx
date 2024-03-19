c05a77ba4d0302f05c30046a98c97502126e5b69
Jefferson Celestino <jefferson.ucb02@gmail.com>
2024-03-19 20:00:40 -0300

    Adding React Router DOM

---
+++ src/routes/ClientHome/Catalog/index.tsx
@@ -0,0 +1,54 @@
+import ButtonNextPage from '../../../components/ButtonNextPage';
+import CatalogCard from '../../../components/CatalogCard';
+import SearchBar from '../../../components/SearchBar';
+import { ProductDTO } from '../../../models/product';
+import './styles.css';
+
+const product: ProductDTO = {
+    id: 2,
+    name: "Smart TV",
+    description: "Computador Gamer XT com suporte e 16GB de memória e processador turbo plus",
+    imgUrl: "https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/2-big.jpg",
+    price: 2500.99,
+    categories: [
+        {
+            id: 2,
+            name: "Eletrônicos"
+        },
+        {
+            id: 3,
+            name: "Computadores"
+        },
+        {
+            id: 4,
+            name: "Importados"
+        }
+    ]
+}
+
+export default function Catalog() {
+    return (
+        <main>
+            <section id="dsc-catalog-section" className="dsc-container">
+                <SearchBar />
+
+                <div className="dsc-catalog-cards dsc-mt20 dsc-mb20">
+                    <CatalogCard product={product} />
+                    <CatalogCard product={product} />
+                    <CatalogCard product={product} />
+                    <CatalogCard product={product} />
+                    <CatalogCard product={product} />
+                    <CatalogCard product={product} />
+                    <CatalogCard product={product} />
+                    <CatalogCard product={product} />
+                    <CatalogCard product={product} />
+                    <CatalogCard product={product} />
+                    <CatalogCard product={product} />
+                    <CatalogCard product={product} />
+                </div>
+
+                <ButtonNextPage text="Carregar mais" />
+            </section>
+        </main>
+    );
+}
