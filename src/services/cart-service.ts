0271180e8d8174c71dce5226e1ce09961cbab654
Jefferson Celestino <jefferson.ucb02@gmail.com>
2024-04-24 17:15:20 -0300

    cart repository return obj Order

--- src/services/cart-service.ts
+++ src/services/cart-service.ts
@@ -13,6 +13,7 @@ export function getCart(): OrderDTO {
 export function addProduct(product: ProductDTO) {
     const cart = cartRepository.get();
     const item = cart.items.find(x => x.productId === product.id);
+    
     if (!item) {
         const newItem = new OrderItemDTO(product.id, 1, product.name, product.price, product.imgUrl);
         cart.items.push(newItem);
