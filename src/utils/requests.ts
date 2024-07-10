9941e5477447cc80d96146ea1a94f0207818d17b
unknown <jefferson.crn@gmail.com>
2024-07-10 20:17:52 -0300

    feat: Request update withCredentials

--- src/utils/requests.ts
+++ src/utils/requests.ts
@@ -1,6 +1,16 @@
 import axios, { AxiosRequestConfig } from "axios";
+import * as authService from "../services/auth-service";
 import { BASE_URL } from "./system";
 
 export function requestBackend(config: AxiosRequestConfig) {
-    return axios({ ...config, baseURL: BASE_URL });
+
+    // the conditions if (ternary)
+    const headers = config.withCredentials
+        ? {
+            ...config.headers,
+            Authorization: "Bearer " + authService.getAccessToken()
+        }
+        : config.headers;
+
+    return axios({ ...config, baseURL: BASE_URL, headers });
 }
