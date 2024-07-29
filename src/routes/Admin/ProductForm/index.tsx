befb5c1b332bb6059113f7d9eef440a417dd1bf0
unknown <jefferson.crn@gmail.com>
2024-07-29 00:51:12 -0300

    feat: FormTextArea

--- src/routes/Admin/ProductForm/index.tsx
+++ src/routes/Admin/ProductForm/index.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 import { Link, useParams } from 'react-router-dom';
 import FormInput from '../../../components/FormInput';
+import FormTextArea from '../../../components/FormTextArea';
 import * as productService from '../../../services/product-service';
 import * as forms from '../../../utils/forms';
 import './styles.css';
@@ -41,6 +42,17 @@ export default function ProductForm() {
             type: "text",
             placeholder: "Imagem",
         },
+        description: {
+            value: "",
+            id: "description",
+            name: "description",
+            type: "text",
+            placeholder: "Descrição",
+            validation: function (value: string) {
+                return /^.{10,}$/.test(value);
+            },
+            message: "Descrição mínima de 10 caracteres",
+        }
     });
 
     useEffect(() => {
@@ -97,6 +109,15 @@ export default function ProductForm() {
                                     onTurnDirty={handleTurnDirty}
                                     onChange={handleInputChange}
                                 />
+                            </div>
+                            <div>
+                                <FormTextArea
+                                    {...formData.description}
+                                    className="dsc-form-control dsc-textarea"
+                                    onTurnDirty={handleTurnDirty}
+                                    onChange={handleInputChange}
+                                />
+                                <div className="dsc-form-error">{formData.description.message}</div>
                             </div>
                         </div>
 
